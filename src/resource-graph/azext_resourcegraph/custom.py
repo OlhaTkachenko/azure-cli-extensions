fbbb37b3d6bc9d317c3263a07bbe755b67431518
Olha Tkachenko <oltkache@microsoft.com>
2019-06-11 12:31:53 -0700

    Consume ResultFormat option

--- src/resource-graph/azext_resourcegraph/custom.py
+++ src/resource-graph/azext_resourcegraph/custom.py
@@ -30,12 +30,13 @@ def execute_query(client, graph_query, first, skip, subscriptions):
             request_options = QueryRequestOptions(
                 top=min(first - len(results), __ROWS_PER_PAGE),
                 skip=skip + len(results),
-                skip_token=skip_token
+                skip_token=skip_token,
+                result_format="objectArray"
             )
             request = QueryRequest(query=graph_query, subscriptions=subs_list, options=request_options)
             response = client.resources(request)  # type: QueryResponse
             skip_token = response.skip_token
-            results.extend(_table_to_dicts(response.data))
+            results.extend(response.data)
 
             if len(results) >= first or skip_token is None:
                 break
@@ -51,18 +52,6 @@ def _get_cached_subscriptions():
 
     cached_subs = Profile().load_cached_subscriptions()
     return [sub['id'] for sub in cached_subs]
-
-
-def _table_to_dicts(table):
-    # type: (Table) -> list[dict]
-
-    results = []
-    for row in table.rows:
-        result = {}
-        for col_index in range(len(table.columns)):
-            result[table.columns[col_index].name] = row[col_index]
-        results.append(result)
-    return results
 
 
 def _to_dict(obj):
